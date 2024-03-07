4db2da1f6a4b25604afd298cf59508a5678c49e6
Guionardo Furlan - Levva <guionardo.furlan.ext@jeitto.com.br>
2024-03-07 20:58:06 -0300

    Fix simple_pickle

--- planetapeia_desfiles/desfiles/views/utils/simple_pickle.py
+++ planetapeia_desfiles/desfiles/views/utils/simple_pickle.py
@@ -1,12 +1,34 @@
+import datetime
 import json
 
-from bson import json_util
+from dateutil.parser import ParserError, parse
 from django.core.serializers.json import DjangoJSONEncoder
 
 
+def object_hook(obj: dict):
+    for key, value in obj.items():
+        if isinstance(value, str):
+            try:
+                obj[key] = parse(value)
+            except ParserError:
+                pass
+
+    return obj
+
+
 def loads(dump: str):
-    return json.loads(dump, object_hook=json_util.object_hook)
+    return json.loads(dump, object_hook=object_hook)
 
 
 def dumps(data: dict) -> str:
     return json.dumps(data, cls=DjangoJSONEncoder)
+
+
+if __name__ == "__main__":
+    sample = {"name": "Guionardo", "date": datetime.datetime(2024, 3, 7, 10, 10, 10)}
+    as_json = dumps(sample)
+    print(as_json)
+
+    sample_copy = loads(as_json)
+    assert sample["name"] == sample_copy["name"]
+    assert sample["date"] == sample_copy["date"]

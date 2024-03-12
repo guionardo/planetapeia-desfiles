234907d35b463d8b2df26f20d78d3b01defa5b82
Guionardo Furlan - Levva <guionardo.furlan.ext@jeitto.com.br>
2024-03-12 02:13:51 -0300

    Registro devolução traje

--- planetapeia_desfiles/desfiles/views/home/__init__.py
+++ planetapeia_desfiles/desfiles/views/home/__init__.py
@@ -10,7 +10,6 @@ from .desfiles_ativos import get_desfiles_ativos
 from .meus_convites import get_meus_convites
 from .revisoes_senha import get_revisoes_senha
 from .trajes import get_trajes
-from .traje_entrega import get_traje_entrega
 
 
 class HomeView(LoginRequiredMixin, TemplateView):
@@ -27,7 +26,6 @@ class HomeView(LoginRequiredMixin, TemplateView):
                 get_revisoes_senha(request),
                 get_meus_convites(request),
                 get_trajes(request),
-                get_traje_entrega(request),
             ],
         }
         return self.render_to_response(context)
